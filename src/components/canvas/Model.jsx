624661210762d8e73e2468076d009a8016492453
PatrickHume <patrick.hume31@gmail.com>
2023-05-14 16:53:32 +0100

    refactor model loading

--- src/components/canvas/Model.jsx
+++ src/components/canvas/Model.jsx
@@ -1,9 +1,13 @@
 import React, { useRef, useEffect } from 'react'
 import { useFBX, useAnimations } from '@react-three/drei'
 
+/* Load an FBX model from ./public */
 export default function Model({ name, scale = 1.0, ...props }) {
+    /* Load model and animations. */
     const fbx = useFBX(`/${name}.fbx`)
+    const { ref, actions, names } = useAnimations(fbx.animations)
 
+    /* Remove shininess from mesh */
     fbx.traverse(child => {
         if (child.isMesh) {
             for (const material of child.material) {
@@ -12,14 +16,12 @@ export default function Model({ name, scale = 1.0, ...props }) {
         }
     })
 
-    const { animations } = useFBX(`/${name}.fbx`)
-
-    const { ref, actions, names } = useAnimations(animations)
-
+    /* Play eating animation. */
     useEffect(() => {
         actions['AnimalArmature|Eating'].play();
     }, []);
 
+    /* Return model */
     return (
         <>
             <primitive
