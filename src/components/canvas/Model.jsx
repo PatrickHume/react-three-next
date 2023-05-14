1a5bfce1e1846ff6554648132f5146c45b99734b
PatrickHume <patrick.hume31@gmail.com>
2023-05-14 16:49:47 +0100

    add animation

--- src/components/canvas/Model.jsx
+++ src/components/canvas/Model.jsx
@@ -1,16 +1,33 @@
-import React, { useRef } from 'react'
-import { useFBX } from '@react-three/drei'
-import { MeshStandardMaterial } from 'three'
+import React, { useRef, useEffect } from 'react'
+import { useFBX, useAnimations } from '@react-three/drei'
 
 export default function Model({ name, scale = 1.0, ...props }) {
     const fbx = useFBX(`/${name}.fbx`)
+
     fbx.traverse(child => {
         if (child.isMesh) {
             for (const material of child.material) {
-                console.log(material)
                 material.shininess = 0.1
             }
         }
     })
-    return <primitive object={fbx} position={[0, 0, 0]} scale={scale} />
+
+    const { animations } = useFBX(`/${name}.fbx`)
+
+    const { ref, actions, names } = useAnimations(animations)
+
+    useEffect(() => {
+        actions['AnimalArmature|Eating'].play();
+    }, []);
+
+    return (
+        <>
+            <primitive
+                ref={ref}
+                object={fbx}
+                position={[0, 0, 0]}
+                scale={scale}
+            />
+        </>
+    );
 }
